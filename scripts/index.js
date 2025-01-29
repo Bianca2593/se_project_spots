1eaf32017bb3f77a9704e250d908eeaaf0bb3614
Bianca Nechita <nechita,bianca@yahoo.com>
2025-01-29 14:16:12 -0700

    Dom

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,18 @@ const initialCards= [
     },
 
 ];
-console.log(initialCards);
+
+const profileEditButton= document.querySelector(".profile__edit-btn");
+const profileEditModal = document.querySelector("#edit-modal");
+const profileEditExitButton = document.querySelector(".modal__close-btn");
+
+function openModal() {
+ editModal.classList.add("modal_opened");
+}
+
+function closeModal(){
+   editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click, openModal");
+editModalCloseBtn.addEventListener("click, closeModal");
